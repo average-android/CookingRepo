0252d363b14931602fccfdf1e1909dfe156be528
david <dmancini87@gmail.com>
2022-11-03 21:01:06 -0400

    tested search

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,21 @@ fetch("https://www.themealdb.com/api/json/v1/1/random.php")
 .then(function (data) {
   console.log(data);
 });
+let = drinkSearch = "martini"
+let mealSearch = "cake"
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkSearch)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
+
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealSearch)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});

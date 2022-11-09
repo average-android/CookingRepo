803e8a33bc26afa1b2ebe905368f78eff145d248
david <dmancini87@gmail.com>
2022-11-08 19:37:39 -0500

    add search bar

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,31 @@
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
 
-fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
-let = drinkSearch = "martini"
-let mealSearch = "cake"
+let drinkSearch = document.querySelector("#drinkrecipeSearch")
+let mealSearch = document.querySelector("#mealrecipeSearch")
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkSearch)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
 
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealSearch)
+drinkButton.addEventListener("click", drinkSubmit) 
+function drinkSubmit (event){
+  event.preventDefault();
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkSearch.value)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) {
   console.log(data);
 });
+}
+;
+
+
+
+mealButton.addEventListener("click", mealSubmit) 
+function mealSubmit (event){
+  event.preventDefault();
+fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealSearch.value)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
+};
